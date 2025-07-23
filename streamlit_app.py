bed7bc16a8a95478920829cc7019dfa71373830d
Lografim <lografim@gmail.com>
2025-07-23 23:38:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
+import pandas as pd
 st.title("Bismillah Mulai Progress Mini FPDC")
 st.subheader("iyh ini kerjaan gua")
 
@@ -7,3 +7,11 @@ st.header("gimana cara buat tabel")
 st.write(
     "Let's start building! For healp and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("""
+# My first app
+Hello *world!*
+""")
+ 
+df = pd.read_csv("my_data.csv")
+st.line_chart(df)

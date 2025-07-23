410f9428b35dfc3c70960786a37a33c0dbb87118
Lografim <lografim@gmail.com>
2025-07-23 23:36:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit as st
-st.header("test")
+
+st.title("Bismillah Mulai Progress Mini FPDC")
+st.subheader("iyh ini kerjaan gua")
+
+st.header("gimana cara buat tabel")
+st.write(
+    "Let's start building! For healp and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
